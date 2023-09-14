a04d4893bf8b65c4e3c7b355d3017882c9a345a4
SamikshaD1 <127653626+SamikshaD1@users.noreply.github.com>
2023-09-14 10:39:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -32,12 +32,25 @@ const lookup = {
 };
 
 function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
+  return encodedString.split('').map(function(char){
+	   if (/[A-Z]/.test(char)) {
+      // Shift the character code by 13 positions, wrapping around if necessary
+      const charCode = char.charCodeAt(0);
+      const shiftedCharCode = charCode >= 78 ? charCode - 13 : charCode + 13;
+      return String.fromCharCode(shiftedCharCode);
+		   } else {
+      // Non-alphabetic characters remain unchanged
+      return char;
+    }
+	  }).join('');
+}// Your Result goes here
   // Only change code below this line
 
   return; //return decodedArr
 }
-
+const encodedString = "GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.";
+const decodedString = rot13(encodedString);
+console.log(decodedString);
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 // console.log(rot13("SERR YBIR? NPPVBWBO"));
